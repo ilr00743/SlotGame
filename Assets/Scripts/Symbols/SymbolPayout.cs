8ffd03ced661a20bf12a2b8fb9f747730f3c84bf
illiaR <ir@readable.games>
2025-01-22 14:29:09 +0200

    Created symbols payout config

---
+++ Assets/Scripts/Symbols/SymbolPayout.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Symbols
+{
+    [Serializable]
+    public class SymbolPayout
+    {
+        public string SymbolName;
+        public int Multiplier;
+    }
+}
---
+++ Assets/Scripts/Symbols/SymbolPayoutConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Symbols
+{
+    [CreateAssetMenu(fileName = "SymbolPayoutConfig", menuName = "SlotMachine/Symbol Payout Config", order = 0)]
+    public class SymbolPayoutConfig : ScriptableObject
+    {
+        [SerializeField] private List<SymbolPayout> _symbolPayoutPairs;
+        
+        public IReadOnlyDictionary<string, int> SymbolPayoutPairs => _symbolPayoutPairs
+            .ToDictionary(key => key.SymbolName, value => value.Multiplier);
+    }
+}
